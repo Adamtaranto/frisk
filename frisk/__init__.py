19946ca1ab943f98d2b54a5b1a252a6204607a7c
Kevin Murray <spam@kdmurray.id.au>
2015-10-27 12:47:06 +1100

    Fix licence, add license header to python files

--- frisk/__init__.py
+++ frisk/__init__.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
-# frisk.py
-# Version 1. Adam Taranto, July 2015
-# Contact, Adam Taranto, adam.taranto@anu.edu.au
+# Copyright (C) 2015 Adam Taranto <adam.p.taranto@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #########################################################################################
 # Detects regions of unusual sequence composition by comparison of local kmer           #
